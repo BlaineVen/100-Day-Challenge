075131e70aff31a7fa57a44ee6fba304cda1f658
Valmitic <51682028+Valmitic@users.noreply.github.com>
2022-01-23 16:59:39 -0500

    p18
    - Added navagation to "wiki" and "something" page

---
+++ p18/app.js
@@ -0,0 +1,14 @@
+
+function myFunction1(){
+    document.getElementById('something').innerHTML=Date()
+}
+
+function myFunction2(){
+    let element = document.body;
+    element.classList.toggle('dark-mode');
+}
+
+function myFunction3(){
+    document.getElementById('');
+}
+
