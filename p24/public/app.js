a3eb4c93bceb0c75fb6ad4ff87d1ca8eb37baf70
Valmitic <51682028+Valmitic@users.noreply.github.com>
2022-02-01 17:25:49 -0500

    p23
    - Changed file directory

---
+++ p24/public/app.js
@@ -0,0 +1,22 @@
+
+function myFunction1(){
+    document.getElementById('something').innerHTML=Date()
+}
+
+function myFunction2(){
+    let element = document.body;
+    element.classList.toggle('dark-mode');
+}
+
+function myFunction3(){
+    document.getElementById('');
+}
+
+//Math
+document.getElementById('math').innerHTML = Math.random();
+
+
+function getRndInteger(min, max) {
+   return Math.floor(Math.random() * 100)
+}
+
