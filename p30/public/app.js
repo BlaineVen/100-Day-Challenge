d7d6ad5a751f2f49240e6a304f44c1a0d23c76f9
Valmitic <51682028+Valmitic@users.noreply.github.com>
2022-02-14 11:28:12 -0500

    p30

---
+++ p30/public/app.js
@@ -0,0 +1,24 @@
+
+function myFunction1(){
+    document.getElementById('something').innerHTML=Date()
+}
+
+function myFunction2(){
+    let element = document.body;
+    element.classList.toggle('dark-mode');
+}
+
+function myFunction3(){
+    document.getElementById('');
+}
+
+
+function getRndInteger(min, max) {
+   return Math.floor(Math.random() * 999)
+}
+
+app.get('/', (req, res) =>{
+    res.send('This does nothing');
+});
+
+console.log('This is a console log');
