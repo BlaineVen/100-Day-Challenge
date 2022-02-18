4d93109a4fcbc0b6d99f7ad9b54afc795da484d6
Valmitic <51682028+Valmitic@users.noreply.github.com>
2022-02-18 16:45:12 -0500

    p32
    - Finally figured out why jQuery wasn't working for me. Speaking of which...
    
    - Added a bit of jQuery

---
+++ p32/public/app.js
@@ -0,0 +1,30 @@
+// Ze Funksionz!
+
+function myFunction1(){
+    document.getElementById('something').innerHTML=Date()
+}
+
+function myFunction2(){
+    let element = document.body;
+    element.classList.toggle('dark-mode');
+}
+
+function myFunction3(){
+    document.getElementById('');
+}
+
+
+function getRndInteger(min, max) {
+   return Math.floor(Math.random() * 999)
+}
+
+
+console.log('This is a console log');
+
+// Da JayQuerie
+
+$(document).ready(function(){
+    $("#howto").click(function(){
+      $(".stuff").show("slow");
+    });
+  });
